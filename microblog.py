e5d675c1647a8ea112d98642e74aa1ec5fb5e03f
risingmoon <justindavidlee88@gmail.com>
2014-03-11 14:55:34 -0700

    Added tests

--- microblog.py
+++ microblog.py
@@ -1,17 +1,46 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = ""
+db = SQLAlchemy(app)
 
 
 def write_post(title, text):
-    pass
+    post = Post(title, text)
+    db.session.add(post)
+    db.session.commit()
 
 
 def read_posts():
     pass
+    #return db.query_all()
 
 
 def read_post(id):
     pass
+    # post = Post.query.filter_by(id=id)
+    # if post:
+    #     return post
+    # else:
+    #     raise EnvironmentError
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.pub_date = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests.py
+++ tests.py
@@ -1,13 +1,23 @@
 import unittest
-from microblog import app
-from flask import url_for
+from microblog import write_post, db, Post
 
 
 class MicroblogTest(unittest.TestCase):
 
-    def test_write_post(self):
-        pass
+    def setUp(self):
+        self.db = db
+        self.db.create_all()
 
+    def tearDown(self):
+        self.db.drop_all()
+
+    def test_empty_database(self):
+        self.assertEquals(len(Post.query.all()), 0)
+
+    def test_write_post(self):
+        write_post("Python", "I LOVE UNIT-TESTING IN PYTHON!!!!")
+        self.assertEquals(len(Post.query.all()), 1)
+    
     def test_read_posts(self):
         pass
 
