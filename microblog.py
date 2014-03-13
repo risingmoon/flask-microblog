09470e025a7c58ead6b4cb43b083dd046a274d3e
risingmoon <justindavidlee88@gmail.com>
2014-03-13 06:10:11 -0700

    Working on login

--- microblog.py
+++ microblog.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import (Flask,
+                   render_template,
+                   request, redirect,
+                   url_for,
+                   session,
+                   flash)
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+#from jinja2 import Template
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///microblog"
+app.config['USERNAME'] = "test"
+app.config['PASSWORD'] = "one"
+app.debug = True
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
@@ -16,7 +26,7 @@ manager.add_command('db', MigrateCommand)
 
 def write_post(title, text):
     if title and text:
-        post = Post(title, text)
+        post = Post(title, text,)
         db.session.add(post)
         db.session.commit()
     else:
@@ -34,34 +44,66 @@ def read_post(id):
         raise KeyError("Error: Key not found")
 
 
-@app.route('/')
-def list_page():
-    pass
+@app.route('/', methods=['GET'])
+def list_view():
+    post_list = read_posts()
+    page_html = render_template('lists.html', posts=post_list)
+    return page_html
 
 
-@app.route('/blog/<id>')
-def details_page():
-    pass
+@app.route('/post/<int:id>/', methods=['GET'])
+def details_view(id):
+    post = read_post(id)
+    page_html = render_template('details.html', post=post)
+    return page_html
 
 
-@app.route('/add')
-def add_page():
-    #if request.method == "GET":
-    #Template Render
-    pass
+@app.route('/add', methods=['GET', 'POST'])
+def add_view():
+    if request.method == "POST":
+        write_post(request.form['title'], request.form['body'])
+        return redirect(url_for('list_view'))
+    else:
+        page_html = render_template('add.html')
+        return page_html
 
 
-class User(db.Model):
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            session['current_user'] = request.form['username']
+            flash('You were logged in')
+            return redirect(url_for('list_view'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('list_view'))
+
+
+class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(30))
+    posts = db.relationship('Post', backref='author',
+                            lazy='dynamic')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Author %r>' % self.username
 
 
 class Post(db.Model):
@@ -70,10 +112,14 @@ class Post(db.Model):
     body = db.Column(db.Text)
     pub_date = db.Column(db.DateTime)
 
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+
     def __init__(self, title, body):
         self.title = title
         self.body = body
         self.pub_date = datetime.utcnow()
+        #self.author_id = Author.query(Author).select_from(Author.username).filter(Author.username==session['current_user'])
+        print Author.query(Author).select_from(Author.username).filter(Author.username==session['current_user'])
 
     def __repr__(self):
         return '<Post %r>' % self.title
--- tests.py
+++ tests.py
@@ -1,17 +1,42 @@
 import unittest
-from microblog import write_post, read_posts, db, Post, read_post
+from flask import request
+from microblog import app, write_post, read_posts, db, Post, read_post
 from sqlalchemy.exc import DataError
 
+# with app.test_client_context()
 
 class MicroblogTest(unittest.TestCase):
 
     def setUp(self):
         self.db = db
         self.db.create_all()
+        self.client = app.test_client()
 
     def tearDown(self):
         self.db.session.remove()
         self.db.drop_all()
+
+    def test_list_view_url(self):
+        with self.client as client:
+            rv = client.get('/')
+            assert request.path == '/'
+
+    def test_list_view(self):
+        title_lists = ['Python', 'Javascript', 'Rails', 'iOS']
+        self.write_posts(title_lists)
+        with self.client as client:
+            rv = client.get('/')
+            for title in title_lists:
+                self.assertIn(title, rv.data)
+
+    def test_details_view(self):
+        with self.client as client:
+            rv = client.get('/posts/1')
+
+    def write_posts(self, title_lists):
+        for title in title_lists:
+            write_post(title, "BLANK BODY")
+
 
     def test_empty_database(self):
         self.assertEquals(len(Post.query.all()), 0)
@@ -43,8 +68,7 @@ class MicroblogTest(unittest.TestCase):
 
     def test_read_posts(self):
         title_lists = ['Python', 'Javascript', 'Rails', 'iOS']
-        for title in title_lists:
-            write_post(title, "BLANK BODY")
+        self.write_posts(title_lists)
         post_list = read_posts()
         title_lists.reverse()
         for index in range(4):
@@ -58,6 +82,16 @@ class MicroblogTest(unittest.TestCase):
             self.assertTrue(read_post(index))
         with self.assertRaises(KeyError):
             read_post(6)
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
 
 
 if __name__ == '__main__':
